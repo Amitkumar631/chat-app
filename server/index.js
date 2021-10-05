051f86720692c89e9377708d00636b386cf857d0
Nikhil <nikhilpradhan363@gmail.com>
2021-10-05 21:38:37 +0530

    index.js updated

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const https = require("https");
 
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.json());
@@ -8,7 +9,13 @@ app.use(express.static(buildPath));
 
 app.post('/send', (req, res) => {
     console.log(req.body);
-    res.send(req.body);
+    const url = "https://random-data-api.com/api/address/random_address";
+    https.get(url, (response) => {
+        response.on("data", (data) => {
+            const address = JSON.parse(data);
+            console.log(address);
+        });
+    });
 });
 
 app.listen(3030, () => {
