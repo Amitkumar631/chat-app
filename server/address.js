34f7e4800c04841ef6591719a520a07b044cfd9c
Nikhil <nikhilpradhan363@gmail.com>
2021-10-13 14:01:59 +0530

    add promise function

--- server/address.js
+++ server/address.js
@@ -2,17 +2,19 @@ import https from "https";
 
 
 export default function address_api (){
-    const address_dict = {};
-    const url = "https://random-data-api.com/api/address/random_address";
-    https.get(url, (response) => {
-        response.on("data", (data) => {
-            const address = JSON.parse(data);
-            address_dict["uid"] = address.uid;
-            address_dict["city"] = address.city;
-            address_dict["postcode"] = address.postcode;
-            address_dict["country"] = address.country;
+    var address_dict = {};
+    return new Promise((resolve) => {
+        const url = "https://random-data-api.com/api/address/random_address";
+        https.get(url, (response) => {
+            response.on("data", (data) => {
+                const address = JSON.parse(data);
+                address_dict["uid"] = address.uid;
+                address_dict["city"] = address.city;
+                address_dict["postcode"] = address.postcode;
+                address_dict["country"] = address.country;
+                resolve(address_dict);
+            });
         });
     });
-    return address_dict;
 }
 
